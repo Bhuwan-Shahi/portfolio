6637d0d2c834540a24cd86ad1060dc1b448578a7
BisekhKarki <karkidai79@gmail.com>
2024-08-02 11:34:57 +0545

    Made some minor changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Frontend Development", "Backend Development", "Web Designing", "Java"],
+    strings: ["Frontend Development", "Java", "Python","Backend Development", "Web Designing" ],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
